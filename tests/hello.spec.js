60c94e6d0c23e4af338a611037674b4fc1f62400
up1 <somkiat.p@gmail.com>
2024-02-21 01:52:06 +0700

    Working with test id attribute

--- tests/hello.spec.js
+++ tests/hello.spec.js
@@ -12,10 +12,25 @@ test('Check h1 text', async ({ page }) => {
   expect(h1Text).toBe('Call REST API');
 });
 
+test('Check h1 text from test id', async ({ page }) => {
+  await page.goto('https://demo-frontend-reactjs.vercel.app/');
+  const message_text = await page.getByTestId('message_text').textContent();
+  expect(message_text).toBe('Call REST API');
+});
+
+
 test('Check p tag text', async ({ page }) => {
   await page.goto('https://demo-frontend-reactjs.vercel.app/');
   
   await page.waitForSelector('p');
   const pText = await page.textContent('p');
   expect(pText).toBe('Hello World!');
+});
+
+test('Check p tag text from test id', async ({ page }) => {
+  await page.goto('https://demo-frontend-reactjs.vercel.app/');
+  
+  await page.waitForSelector('[data-testid="hello_text"]');
+  const hello_text = await page.getByTestId('hello_text').textContent();
+  expect(hello_text).toBe('Hello World!');
 });
